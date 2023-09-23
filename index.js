856ec2125dc8adf6928f61c594ae04de678d2f96
Lionel Ringenbach <ucodia@live.com>
2023-09-22 17:45:19 -0700

    Run prettier

--- index.js
+++ index.js
@@ -1,85 +1,82 @@
 const createLcg = (seed, modulus, multiplier, increment) => {
-    const m = modulus
-    const a = multiplier
-    const c = increment
-    let z = seed
-  
-    return {
-      rand: () => {
-        z = (a * z + c) % m
-        return z / m
-      }
-    }
-  }
-  
+  const m = modulus;
+  const a = multiplier;
+  const c = increment;
+  let z = seed;
+
+  return {
+    rand: () => {
+      z = (a * z + c) % m;
+      return z / m;
+    },
+  };
+};
+
 const getSeedsFromDate = (date) => {
-  const year = date.getFullYear()
-  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date
     .getDate() // day of the month
     .toString()
-    .padStart(2, '0')
-  const hour = date
-    .getHours()
-    .toString()
-    .padStart(2, '0')
+    .padStart(2, "0");
+  const hour = date.getHours().toString().padStart(2, "0");
 
   return {
     minute: parseInt(`${year}${month}${day}${hour}`),
-    hour: parseInt(`${year}${month}${day}`)
-  }
-}
+    hour: parseInt(`${year}${month}${day}`),
+  };
+};
 
 const getSequenceFromLcg = (lcg, length) => {
-  const values = []
+  const values = [];
 
   // generate sequence
   for (let i = 0; i < length; i++) {
-    values.push(lcg.rand())
+    values.push(lcg.rand());
   }
 
   // copy and sort sequence
-  const sorted = values.slice().sort()
+  const sorted = values.slice().sort();
 
   // normalize values based on their order
   for (let i = 0; i < sorted.length; i++) {
-    values[values.indexOf(sorted[i])] = i
+    values[values.indexOf(sorted[i])] = i;
   }
 
-  return values
-}
+  return values;
+};
 
 const mergeTimeWithDate = (time, date) => {
-  const flowdate = new Date(date.getTime())
-  flowdate.setHours(time.hour)
-  flowdate.setMinutes(time.minute)
-  flowdate.setSeconds(time.second)
-  return flowdate
-}
+  const flowdate = new Date(date.getTime());
+  flowdate.setHours(time.hour);
+  flowdate.setMinutes(time.minute);
+  flowdate.setSeconds(time.second);
+  return flowdate;
+};
 
 export const fromDate = (date) => {
   // extract seeds
-  const seed = getSeedsFromDate(date)
+  const seed = getSeedsFromDate(date);
 
   // create hours LCG using values from MINSTD
-  const mLcg = createLcg(seed.minute, 2147483647, 48271, 0)
-  
+  const mLcg = createLcg(seed.minute, 2147483647, 48271, 0);
+
   // create hours LCG using values from the Numerical Recipes book
-  const hLcg = createLcg(seed.hour, 4294967296, 1664525, 1013904223)
+  const hLcg = createLcg(seed.hour, 4294967296, 1664525, 1013904223);
 
   // generate sequences
-  const mSequence = getSequenceFromLcg(mLcg, 60)
-  const hSequence = getSequenceFromLcg(hLcg, 24)
+  const mSequence = getSequenceFromLcg(mLcg, 60);
+  const hSequence = getSequenceFromLcg(hLcg, 24);
 
   // build flowtime
   const time = {
     hour: hSequence[date.getHours()],
     minute: mSequence[date.getMinutes()],
-    second: date.getSeconds()
-  }
+    second: date.getSeconds(),
+  };
 
   return {
     ...time,
-    toDate: () => mergeTimeWithDate(time, date)
-  }
-}
+    toDate: () => mergeTimeWithDate(time, date),
+  };
+};
--- index.test.js
+++ index.test.js
@@ -1,56 +1,56 @@
-import { fromDate } from '.'
+import { fromDate } from ".";
 
-describe('flowtime', () => {
-  it('should map to known flowtime values', () => {
+describe("flowtime", () => {
+  it("should map to known flowtime values", () => {
     const actuals = [
-      new Date('1901-10-18T06:12:37'),
-      new Date('2069-04-20T04:20:00'),
-      new Date('1975-05-01T12:54:09'),
-      new Date('1988-09-30T10:59:46'),
-      new Date('2052-01-01T18:37:19')
-    ].map(fromDate)
+      new Date("1901-10-18T06:12:37"),
+      new Date("2069-04-20T04:20:00"),
+      new Date("1975-05-01T12:54:09"),
+      new Date("1988-09-30T10:59:46"),
+      new Date("2052-01-01T18:37:19"),
+    ].map(fromDate);
 
     const expected = [
       { hour: 5, minute: 3, second: 37 },
       { hour: 5, minute: 57, second: 0 },
       { hour: 13, minute: 31, second: 9 },
       { hour: 21, minute: 56, second: 46 },
-      { hour: 9, minute: 51, second: 19 }
-    ]
+      { hour: 9, minute: 51, second: 19 },
+    ];
 
-    expect(actuals).toMatchObject(expected)
-  })
+    expect(actuals).toMatchObject(expected);
+  });
 
-  it('should return every hour and minute combinations of flowtime only once per day', () => {
-    const actuals = []
-    const expected = []
+  it("should return every hour and minute combinations of flowtime only once per day", () => {
+    const actuals = [];
+    const expected = [];
 
     for (let h = 0; h < 24; h++) {
       for (let m = 0; m < 60; m++) {
-        const hh = h.toString().padStart(2, '0')
-        const mm = m.toString().padStart(2, '0')
-        actuals.push(fromDate(new Date(`2000-01-01T${hh}:${mm}:00`)))
-        expected.push({ hour: h, minute: m, second: 0 })
+        const hh = h.toString().padStart(2, "0");
+        const mm = m.toString().padStart(2, "0");
+        actuals.push(fromDate(new Date(`2000-01-01T${hh}:${mm}:00`)));
+        expected.push({ hour: h, minute: m, second: 0 });
       }
     }
 
     const actualsSort = actuals.sort(
-      (a, b) => a.hour - b.hour || a.minute - b.minute || a.second - b.second
-    )
+      (a, b) => a.hour - b.hour || a.minute - b.minute || a.second - b.second,
+    );
 
-    expect(actualsSort).toMatchObject(expected)
-  })
+    expect(actualsSort).toMatchObject(expected);
+  });
 
-  it('should convert flowtime to date object', () => {
-    const now = new Date()
-    const flowtime = fromDate(now)
-    const flowdate = flowtime.toDate()
+  it("should convert flowtime to date object", () => {
+    const now = new Date();
+    const flowtime = fromDate(now);
+    const flowdate = flowtime.toDate();
 
-    expect(flowdate.getFullYear()).toEqual(now.getFullYear())
-    expect(flowdate.getMonth()).toEqual(now.getMonth())
-    expect(flowdate.getDate()).toEqual(now.getDate())
-    expect(flowdate.getHours()).toEqual(flowtime.hour)
-    expect(flowdate.getMinutes()).toEqual(flowtime.minute)
-    expect(flowdate.getSeconds()).toEqual(flowtime.second)
-  })
-})
+    expect(flowdate.getFullYear()).toEqual(now.getFullYear());
+    expect(flowdate.getMonth()).toEqual(now.getMonth());
+    expect(flowdate.getDate()).toEqual(now.getDate());
+    expect(flowdate.getHours()).toEqual(flowtime.hour);
+    expect(flowdate.getMinutes()).toEqual(flowtime.minute);
+    expect(flowdate.getSeconds()).toEqual(flowtime.second);
+  });
+});
